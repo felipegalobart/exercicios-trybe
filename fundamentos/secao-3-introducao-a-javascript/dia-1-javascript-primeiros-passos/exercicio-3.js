ecd6d8eea1f84b91c00a13b69e90b65b125819d5
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-05-02 19:30:17 -0300

    Exercício 3

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-3.js
@@ -0,0 +1,11 @@
+let numeroA = 100;
+let numeroB = 200;
+let numeroC = 400;
+
+if (numeroA > numeroB && numeroA > numeroC) {
+  console.log(numeroA);
+} else if (numeroB > numeroA && numeroB > numeroC) {
+  console.log(numeroB);
+}else{
+  console.log(numeroC);
+}
